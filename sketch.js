81e3b69057be2d0344d64162dc9a0a6e1955f267
PaulAndrianoff <pandrianoff@gmail.com>
2019-02-05 11:15:35 +0100

    controle palette

--- sketch.js
+++ sketch.js
@@ -18,10 +18,16 @@ let cercleR = 5;
 let colorSeed = 0;
 let handleSeed = 0;
 let stickSeed = 0;
+let currentArray;
 
 // Function pour precharger l'image
 function preload() {
     webImage = loadImage("https://i.ibb.co/1RkJ9Zv/cercle.png");
+}
+
+function setPalletteColor() {
+    randomSeed(new Date().valueOf());
+    currentArray = Math.round(random(colorArray.length))%colorArray.length;
 }
 
 function setcolorSeed() {
@@ -51,6 +57,8 @@ function setup() {
     let colorArrayPalette3 = [color(0, 0, 0), color(30, 25, 55), color(112, 17, 45), color(208, 178, 40)];
     let colorArrayPalette4 = [color(0, 0, 0), color(94, 154, 62), color(106, 109, 104), color(230, 230, 230)];
     colorArray = [colorArrayPalette1, colorArrayPalette2, colorArrayPalette3, colorArrayPalette4];
+    
+    setPalletteColor();
 
     createCanvas(canvasWidth, canvasHeight);
     background(bgColor);
@@ -62,16 +70,22 @@ function draw() {
     background(bgColor);
     noStroke();
 
+    drawSquare();
+    drawHandle();
+    drawLine();
+
+    image(webImage, 0, 0, webImage.width * 1.67, webImage.height * 1.67);
+}
+
+function drawSquare() {
     randomSeed(colorSeed);
     //choix de la palette
-    let currentArray = Math.round(random(colorArray.length))%colorArray.length;
 
     for (let i = 0; i < rowCount; i++) {
-        let y = startY + i * rectHeight;
-
         for (let j=0; j < columnCount; j++) {
             // placement des carrés
             let x = startX + j * rectWidth;
+            let y = startY + i * rectHeight;
 
             //sélection de la couleur
             let currentPalette = Math.round(random(colorArray[currentArray].length))%colorArray[currentArray].length;
@@ -84,15 +98,14 @@ function draw() {
             } else {
                 fill(currentColor);
                 rect(x, y, rectWidth, rectHeight);
-                
             }
 
             x += rectWidth; 
-            
         }
- 
     }
+}
 
+function drawHandle() {
     randomSeed(handleSeed);
     for (let i = 0; i < rowCount; i++) {
         let y = startY + i * rectHeight;
@@ -117,8 +130,9 @@ function draw() {
             }
         }
     }
+}
 
-    // lines settings
+function drawLine() {
     let nbLine = rowCount;
     stroke(0);
     strokeWeight(strokeW);
@@ -142,15 +156,15 @@ function draw() {
         // draw horizontales lines
         line(startX + lineStart * rectWidth, startY + rectHeight * k, startX + lineWidth * rectWidth, startY + rectHeight * k);
     }
-
-    image(webImage, 0, 0, webImage.width * 1.67, webImage.height * 1.67);
-    
 }
 
 function keyTyped() {
     if (key == 'r') {
         document.location.reload;
         console.log('r');
+    }
+    if (key == 'a') {
+        setPalletteColor();
     }
     if (key == 'b') {
         setstickSeed();
